a24be50f28e2b77b43e4ad97df4fb0da54d07c4b
GaudeSoft <78844968+GaudeSoft@users.noreply.github.com>
2022-05-25 13:15:25 +0100

    installation du package publish

--- resources/views/layouts/inc/sidebar.blade.php
+++ resources/views/layouts/inc/sidebar.blade.php
@@ -9,7 +9,22 @@
     <hr class="horizontal light mt-0 mb-2">
     <div class="collapse navbar-collapse  w-auto h-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
-        
+      <li class="nav-item {{ Request::is('#') ? 'active':''}}">
+          <a class="nav-link text-white " href="#">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">list</i>
+            </div>
+            <span class="nav-link-text ms-1">Liste des clients</span>
+          </a>
+        </li>
+        <li class="nav-item {{ Request::is('#') ? 'active':''}}">
+          <a class="nav-link text-white " href="#">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">list</i>
+            </div>
+            <span class="nav-link-text ms-1">Liste des producteurs</span>
+          </a>
+        </li>
         <li class="nav-item {{ Request::is('#') ? 'active':''}}">
           <a class="nav-link text-white " href="{{ route('user.profile') }}">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
